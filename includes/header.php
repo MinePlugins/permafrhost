239b16df9cb827320ca485cf5e59ea08713d0f5d
MinePlugins <quentin.co@free.fr>
2019-01-28 22:48:40 +0100

    Première ébauche PermaFrHost

---
+++ includes/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PermaFrHost</title>
+	<link href="assets/style.css" media="screen, projection" rel="stylesheet" type="text/css" />
+	<link href="assets/owl.carousel.min.css" rel="stylesheet" type="text/css" />
+	<link href="assets/owl.theme.default.min.css" rel="stylesheet" type="text/css" />
+	<link href="assets/style.css" media="screen, projection" rel="stylesheet" type="text/css" />
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+	<script type="text/javascript" src="assets/owl.carousel.min.js"></script>
+
+</head>
+<body>
+
---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+	require_once 'includes/header.php';
+?>
+<link href='https://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
+<div class="arriere-plan">
+
+	<div class="navbar">
+		<div class="navitem"><img class="logo" src="images/Logo.png" /></div>
+		<div class="espace"></div>
+		<div class="active navitem">Home</div>
+		<div class="navitem">Site Web</div>
+		<div class="navitem">Hebergement</div>
+		<div class="navitem">Application</div>
+		<div class="navitem">referencement</div>
+		<div class="navitem">Contact</div>
+	</div>
+	<div class="slider">
+		<div class="owl-carousel owl-theme">
+			 <div class="item">
+			 	<h1>Créer ton site en 3 étapes</h1>
+			 </div>
+			 <div class="item">
+			 	<p>BlaBla</p>
+			 	
+			 </div>
+			 <div class="item">
+			 	<p>BlaBla</p>
+			 	
+			 </div>			 
+		</div>
+	</div>
+	<div id='etoile'></div>
+	<div id='etoile2'></div>
+	<div id='etoile3'></div>
+</div>
+
+<script type="text/javascript">
+	$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    autoplay:true,
+    items:1,
+    nav:false,
+
+});
+</script>
